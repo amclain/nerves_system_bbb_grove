4849cbd5d27dead3b2130b385b0acd390ea051cf
Alex McLain <alex@alexmclain.com>
2020-04-02 23:27:29 -0700

    Merge changes from upstream.

--- mix.exs
+++ mix.exs
@@ -48,9 +48,9 @@ defmodule NervesSystemBbb.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.5.0", runtime: false},
-      {:nerves_system_br, "1.10.2", runtime: false},
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.2.0", runtime: false},
+      {:nerves, "~> 1.5.4 or ~> 1.6.0", runtime: false},
+      {:nerves_system_br, "1.11.0", runtime: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 1.3.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
@@ -66,7 +66,7 @@ defmodule NervesSystemBbb.MixProject do
     [
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+      links: %{"GitHub" => "https://github.com/nerves-project/#{@app}"}
     ]
   end
 
